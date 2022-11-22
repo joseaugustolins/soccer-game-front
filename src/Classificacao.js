8342c371b55867a7f93547667f8235879927a95e
Jose Augusto <joseaugustolins@gmail.com>
2022-11-21 22:36:24 -0300

    adicao de features

--- src/Classificacao.js
+++ src/Classificacao.js
@@ -3,6 +3,8 @@ import ClassificacaoService from "./ClassificacaoService";
 
 const Classificacao = () => {
   const [classificacaoList, setClassificacaoList] = useState([])
+  const [placarJogoList, setPlacarJogoList] = useState([])
+  const [placarJogadorList, setPlacarJogadorList] = useState([])
   var posicao = 0;
   const classificacaoService = ClassificacaoService();
 
@@ -13,13 +15,43 @@ const Classificacao = () => {
         setClassificacaoList(data)
     }
 
+    async function carregaPlacarJogo(){
+      // setClassificacaoList(classificacaoService.listarClassificacao()) 
+      const data = await classificacaoService.listarPlacarJogo()
+      console.log(data)
+      setPlacarJogoList(data)
+  }
+
+
+  async function carregaPlacarJogador(){
+    // setClassificacaoList(classificacaoService.listarClassificacao()) 
+    const data = await classificacaoService.listarPlacarJogador()
+    console.log(data)
+    setPlacarJogadorList(data)
+}
+
+
   return (
-    <div>
-        <button onClick={()=> carregaClassificacao()}>Carregar</button>
+    <div><button onClick={()=> carregaClassificacao()}>Classificacao</button><button onClick={()=> carregaPlacarJogo()}>Placar Jogo</button><button onClick={()=> carregaPlacarJogador()}>Placar Jogador</button>
+        <div>
+        
         
         {classificacaoList && classificacaoList.map(i => (
           <li>{++posicao} {i.nome} - {i.valorResultadoTotal}</li>
       ))}
+      
+      </div>
+      <div>
+      {placarJogoList && placarJogoList.map(i => (
+          <li> {i.nomeJogador} - {i.palpite1} - {i.descricaoJogo} - {i.palpite2} = {i.valor}</li>
+      ))}
+      </div>
+
+      <div>
+      {placarJogadorList && placarJogadorList.map(i => (
+          <li> {i.nomeJogador} - {i.palpite1} - {i.descricaoJogo} - {i.palpite2} = {i.valor}</li>
+      ))}
+      </div>
     </div>
   );
 }
--- src/ClassificacaoService.js
+++ src/ClassificacaoService.js
@@ -8,12 +8,26 @@ export default function ClassificacaoService() {
     //   const [classificacaoList, setClassificacaoList] = useState([])
 
       const listarClassificacao = async () => {
-        const data = await axios.get('http://132.226.160.86:8080/bolao/classificacao')
+        const data = await axios.get('https://132.226.160.86:8080/bolao/classificacao')
+        // const data = await axios.get('http://132.226.160.86:8080/bolao/classificacao')
         return data.data;
       }
 
-          
-      return {listarClassificacao}
+
+      const listarPlacarJogo = async () => {
+        const data = await axios.get('https://132.226.160.86:8080/bolao/placar/jogo/8')
+        // const data = await axios.get('http://132.226.160.86:8080/bolao/classificacao')
+        return data.data;
+      }
+
+      const listarPlacarJogador = async () => {
+        const data = await axios.get('https://132.226.160.86:8080/bolao/placar/jogador/1')
+        // const data = await axios.get('http://132.226.160.86:8080/bolao/classificacao')
+        return data.data;
+      }
+
+      
+      return {listarClassificacao, listarPlacarJogo, listarPlacarJogador}
 }
 
 
