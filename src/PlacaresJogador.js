8e9368308eab05f5b9615c6933b657f13d596ead
Jose Augusto <joseaugustolins@gmail.com>
2022-11-23 00:25:39 -0300

    teste aws

---
+++ src/PlacaresJogador.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+function PlacaresJogador(){
+    return (
+        <div>
+            <h1>Placares Jogador</h1>         
+        </div>
+    )   
+}
+ 
+export default PlacaresJogador;
---
+++ src/PlacaresJogo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+function PlacaresJogo(){
+    return (
+        <div>
+            <h1>Placares Jogo</h1>         
+        </div>
+    )   
+}
+ 
+export default PlacaresJogo;
