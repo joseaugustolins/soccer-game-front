13aed77102ba05c130850012b1ef5835a969bdf3
Jose Augusto <joseaugustolins@gmail.com>
2022-11-23 01:18:45 -0300

    retirando https

--- src/ClassificacaoService.js
+++ src/ClassificacaoService.js
@@ -8,7 +8,7 @@ export default function ClassificacaoService() {
     //   const [classificacaoList, setClassificacaoList] = useState([])
 
       const listarClassificacao = async () => {
-        const data = await axios.get('https://132.226.160.86:8080/bolao/classificacao')
+        const data = await axios.get('http://132.226.160.86:8080/bolao/classificacao')
         // const data = await axios.get('http://132.226.160.86:8080/bolao/classificacao')
         return data.data;
       }
@@ -16,13 +16,13 @@ export default function ClassificacaoService() {
 
       const listarPlacarJogo = async (idJogo) => {
         // const data = await axios.get(`https://132.226.160.86:8080/bolao/placar/jogo/${idJogo}`)
-        const data = await axios.get(`https://132.226.160.86:8080/bolao/placar/jogo/31`)
+        const data = await axios.get(`http://132.226.160.86:8080/bolao/placar/jogo/31`)
         
         return data.data;
       }
 
       const listarPlacarJogador = async (idJogador) => {
-        const data = await axios.get(`https://132.226.160.86:8080/bolao/placar/jogador/${idJogador}`)
+        const data = await axios.get(`http://132.226.160.86:8080/bolao/placar/jogador/${idJogador}`)
         // const data = await axios.get('http://132.226.160.86:8080/bolao/classificacao')
         return data.data;
       }
